13209996aa03078d56224020d9cc63761298f3ae
fefer714 <fernandosoldera@hotmail.com>
2017-05-30 23:32:13 -0300

    final menu

--- JS/home.js
+++ JS/home.js
@@ -1,9 +1,40 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+function openMenuMain() {
+    document.getElementById("menuMain").style.width = "350px";
 }
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+function closeMenuMain() {
+    document.getElementById("menuMain").style.width = "0";
 }
+
+function openMenuWeapons() {
+    document.getElementById("menuWeapons").style.width = "350px";
+}
+
+function closeMenuWeapons() {
+    document.getElementById("menuWeapons").style.width = "0";
+}
+
+function openMenuMaps() {
+    document.getElementById("menuMaps").style.width = "350px";
+}
+
+function closeMenuMaps() {
+    document.getElementById("menuMaps").style.width = "0";
+}
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1	
+        },
+        1000:{
+            items:3
+        }
+    }
+})
